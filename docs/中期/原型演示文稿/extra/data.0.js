de68585b913734858ad68221024f2e727156a26c
rczDS <rcz19@mails.tsinghua.edu.cn>
2021-12-05 14:39:50 +0800

    main

---
+++ docs/中期/原型演示文稿/extra/data.0.js
@@ -0,0 +1,1 @@
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"5831ede18c8330a4195c9434be86fdcd54879208","projectEncryptedPassword":"81dc9bdb52d04dc20036dbd8313ed055","isPasswordRequired":true,"is_on_premises":false,"is_wonder_share":false};
